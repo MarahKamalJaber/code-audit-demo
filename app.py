07ca3fd30273174a10da0c8dcd7e60913c2ea725
Marah Jaber <78085703+MarahKamalJaber@users.noreply.github.com>
2026-04-13 22:41:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
+from flask import Flask, request
 import sqlite3
 
+app = Flask(__name__)
+
+@app.route("/login")
 def login():
+    username = request.args.get("username")
+    password = request.args.get("password")
+
     conn = sqlite3.connect("test.db")
     cursor = conn.cursor()
-
-    username = input("Enter username: ")
-    password = input("Enter password: ")
 
     # ❌ SQL Injection vulnerability
     query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'"
 
     cursor.execute(query)
-    result = cursor.fetchall()
+    return "Done"
 
-    if result:
-        print("Login success")
-    else:
-        print("Login failed")
-
-login()
+app.run()
